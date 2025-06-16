67a0be5ad46aab1e7cec05bc8e62d4d6611b4f64
Mudit Budhdev <mudit.budhdev@gmail.com>
2025-06-16 13:59:09 -0700

    deleted and added some stuff

--- pi_projects/mysitea.py
+++ pi_projects/mysitea.py
@@ -4,6 +4,7 @@ from time import sleep
 import machine
 import rp2
 import sys
+import utime
 
 ssid = ""
 password = ''
@@ -14,49 +15,49 @@ def connectToWifi(): # function to connect to wifi
     # a network is a set of interconnected devices. 
     #a LAN (local area network) is a set of physically connected devices that can communicate with each other.
     # a WLAN is just a LAN, but now instead of physically connected, they are connected wirelessly
-    wlan.active(True) # making the wlan active 
-    wlan.connect(ssid, password)   # connecting the wlan to the internet, so it can communicate wirelessly
-    while wlan.isconnected() == False:  # if the network is not connected to the wifi, repeat this block of code
-        if rp2.bootsel_button() == 1:
-            sys.exit()
-            theLed.value(1)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    while not wlan.isconnected():
+        if rp2.bootsel_button():
+            theLED.value(1)
             utime.sleep(2)
             theLED.value(0)
-        print("Waiting to connect") 
+            sys.exit()
+        print("Waiting to connect")
         sleep(1)
+
     print("Connected!")
     theLED.value(1)
-    print(wlan.ifconfig())
     ip = wlan.ifconfig()[0]
+    print(f"Connected on {ip}")
     return ip
 
 # whats happening now is the rasberry pi is talking to the router, which is talking to the computer 
 # when opening a web page, the computer talks the router, which then talks to a port on the server
 # then, once the router has made a stable connection with the port, then the webpage info can be sent to the cmoputer 
 def open_socket(ip): # creates the socket in the server, where the connection will be made 
-    address = (ip, 80) # contains the name of the device the server needs to talk, also includes the port number 
+    address = (ip, 80)
     connection = socket.socket()
     connection.bind(address)
-    connection.listen(1)  # all this code is just opening up the socket 
-    return connection    
+    connection.listen(1)
+    return connection 
 
 def webpage():
-    html = f"""
+    html = """
     <!DOCTYPE html>
     <html>
     <head>
     <title>Mudit Budhdev</title>
     </head>
     <body>
-
     <h1>Hi!</h1>
     <p>My name is Mudit Budhdev.</p>
-    <p>I coded this website using HTML, and host it on my Rasberry Pi Pico 2 W at home.</p>
-
+    <p>I coded this website using HTML, and host it on my Raspberry Pi Pico 2 W at home.</p>
     </body>
     </html>
     """
-    return str(html)
+    return html
 
 def serve(connection):
         while True:
@@ -69,6 +70,7 @@ def serve(connection):
             client.sendall(html)
 
             client.close()
+            utime.sleep(2)
 
 
 
